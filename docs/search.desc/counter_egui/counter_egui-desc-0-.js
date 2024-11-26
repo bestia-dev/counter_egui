2859fd9412643841be0f61e24ebf78c1dfaf4c8e
bestia.dev <info@bestia.dev>
2024-11-26 14:12:27 +0000

    update docs

---
+++ docs/search.desc/counter_egui/counter_egui-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("counter_egui", 0, "Returns the argument unchanged.\nCalls <code>U::from(self)</code>.")
---
+++ docs/trait.impl/eframe/epi/trait.App.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"counter_egui":[["impl App for <a class=\"struct\" href=\"counter_egui/struct.MyApp.html\" title=\"struct counter_egui::MyApp\">MyApp</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
